petabyt/chdk_mirror

commit 549c8aceef7927d9231f9df464e793cd5d4548ec

Alpha a810 100b and 100d ports from nafraf in http://chdk.setepontos.com/index.php?topic=650.840
git-svn-id: https://subversion.assembla.com/svn/chdk@2168 6794e30b-3f2a-0410-a806-a2bbca1c07ff

trunk/platform/a810/lib.c
@@ -1,0 +1,164 @@
+#include "platform.h"
+#include "lolevel.h"
+
+#define LED_PR 0xC0220120
+static char*    frame_buffer[2];
+
+void shutdown()
+{
+	volatile long *p = (void*)LED_PR;
+
+	asm(
+		"MRS     R1, CPSR\n"
+		"AND     R0, R1, #0x80\n"
+		"ORR     R1, R1, #0x80\n"
+		"MSR     CPSR_cf, R1\n"
+		:::"r1","r0");
+
+	*p = 0x44;  // power off.
+
+	while(1);
+}
+
+void debug_led(int state)
+{
+	// using power LED, which defaults to on
+	// for debugging turn LED off if state is 1 and on for state = 0
+	// leaves LED on at end of debugging
+    volatile long *p = (void*)LED_PR;
+     *(int*)LED_PR=state ? 0x46 : 0x44;
+}
+
+// A810 has two 'lights' - Power LED, and AF assist lamp
+// Power Led = first entry in table (led 0)
+// AF Assist Lamp = second entry in table (led 1) ????
+void camera_set_led(int led, int state, int bright) {
+ static char led_table[2]={0,4};
+ _LEDDrive(led_table[led%sizeof(led_table)], state<=1 ? !state : state);
+}
+
+void shutdown_soft()
+{
+   _PostLogicalEventForNotPowerType(0x1005,0);
+}
+
+int vid_get_viewport_width()
+{
+    // viewport width table for each image size
+    // 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1
+    static long vp_w[4] = { 360, 360, 360, 360 };
+    return vp_w[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
+}
+
+long vid_get_viewport_height()
+{
+    // viewport height table for each image size
+    // 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1
+    static long vp_h[4] = { 240, 240, 240, 240 };
+    return vp_h[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
+}
+
+int vid_get_viewport_xoffset() 
+{
+    // viewport width offset table for each image size
+    // 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1
+    static long vp_w[4] = { 0, 0, 0, 0 };               // should all be even values for edge overlay
+    return vp_w[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
+}
+
+int vid_get_viewport_yoffset() 
+{
+    // viewport height offset table for each image size
+    // 0 = 4:3, 1 = 16:9, 2 = 3:2, 3 = 1:1
+    static long vp_h[4] = { 0, 0, 0, 0 };
+    return vp_h[shooting_get_prop(PROPCASE_ASPECT_RATIO)];
+}
+
+void vid_bitmap_refresh() {
+
+    extern int full_screen_refresh;
+    extern void _LockAndRefresh();      // wrapper function for screen lock
+    extern void _UnlockAndRefresh();    // wrapper function for screen unlock
+
+    full_screen_refresh |= 3;
+    _LockAndRefresh();
+    _UnlockAndRefresh();
+}
+
+
+#ifdef CAM_LOAD_CUSTOM_COLORS
+// Function to load CHDK custom colors into active Canon palette
+void *vid_get_bitmap_active_palette() {
+        extern int active_palette_buffer;
+        extern char* palette_buffer[];
+        return (palette_buffer[active_palette_buffer]+8);
+}
+
+void load_chdk_palette() {
+
+        extern int active_palette_buffer;
+        // Only load for the standard record and playback palettes
+        if ((active_palette_buffer == 0) || (active_palette_buffer == 4))
+        {
+                int *pal = (int*)vid_get_bitmap_active_palette();
+                if (pal[CHDK_COLOR_BASE+0] != 0x33ADF62)
+                {
+                
+                        pal[CHDK_COLOR_BASE+0]  = 0x33ADF62;  // Red
+                        pal[CHDK_COLOR_BASE+1]  = 0x326EA40;  // Dark Red
+                        pal[CHDK_COLOR_BASE+2]  = 0x34CD57F;  // Light Red
+                        pal[CHDK_COLOR_BASE+3]  = 0x373BFAE;  // Green
+                        pal[CHDK_COLOR_BASE+4]  = 0x34BD6CA;  // Dark Green
+                        pal[CHDK_COLOR_BASE+5]  = 0x395AB95;  // Light Green
+                        pal[CHDK_COLOR_BASE+6]  = 0x34766F0;  // Blue
+                        pal[CHDK_COLOR_BASE+7]  = 0x31250F3;  // Dark Blue
+                        pal[CHDK_COLOR_BASE+8]  = 0x37F408F;  // Cyan
+                        pal[CHDK_COLOR_BASE+9]  = 0x3512D5B;  // Magenta
+                        pal[CHDK_COLOR_BASE+10] = 0x3A9A917;  // Yellow
+                        pal[CHDK_COLOR_BASE+11] = 0x3819137;  // Dark Yellow
+                        pal[CHDK_COLOR_BASE+12] = 0x3DED115;  // Light Yellow
+
+                        extern char palette_control;
+                        palette_control = 1;
+                        vid_bitmap_refresh();
+        }
+    }
+}
+#endif
+
+// Functions for PTP Live View system
+int vid_get_palette_type()                      { return 3; }
+int vid_get_palette_size()                      { return 256 * 4; }
+
+void *vid_get_bitmap_active_buffer()
+{
+    return (void*)(*(int*)(0x5400+0x18)); //found @ loc_ff909bb0 a810 100b
+}
+
+// Funtion missing in a810, necessary for games
+static unsigned int random_variable;
+void *_srand(unsigned int seed) {
+    random_variable = seed;
+    return (void *) &random_variable;
+}
+
+int _rand(void) {
+    int value;
+    void * ptr = (void*) (&random_variable);
+    
+    asm volatile (
+      "LDR     R1, %[input] \n"
+      "LDR     R2, =0x41C64E6D \n"
+      "LDR     R0, [R1] \n"
+      "MUL     R0, R2, R0 \n"
+      "ADD     R0, R0, #0x3000 \n"
+      "ADD     R0, R0, #0x39 \n"
+      "STR     R0, [R1] \n"
+      "MVN     R1, #0x8000 \n"
+      "AND     %[result], R1, R0, LSR #0x10 \n"
+      : [result]"=r"   (value)
+      : [input]"m"  (ptr)
+      : /*No clobbers*/
+	);
+    return value;
+};

trunk/platform/a810/sub/100b/lib.c
@@ -1,0 +1,47 @@
+#include "platform.h"
+
+char *hook_raw_image_addr()     {return (char*) 0x420B7FC0;}   //Found @0xffae0ef4 a810
+char *hook_alt_raw_image_addr() {return (char*) 0x420B7FC0;}   //Found @0xffae0ef4 a810
+
+#if 0
+//With this implementation, live view works only in some capture modes.
+//Some overlay objects have wrong color
+void *vid_get_viewport_live_fb()
+{
+    return (void*)(*(int*)(0x2094+0x138));		//0xff848398 [0xff8488d8] + 0xff8483f0 a810
+}
+#else
+//With this implementation, live view works in all capture modes
+void *vid_get_viewport_live_fb()
+{
+    return 0x0;
+/*    void **fb=(void **)0x2094;
+    unsigned char buff = *((unsigned char*)0);
+    if (buff == 0) {
+        buff = 2;
+    }
+    else {
+        buff--;
+    }
+    return fb[buff];
+*/
+}
+#endif
+
+/*
+ * Note copied from SX110 IS
+ * GetBatteryTemperature usually will get back temperature of battery compartment/batteries. GetBatteryTemperature is implemented in Canon's firmware for SX120IS.
+ * Firmware entry point is identified (0xFFC394D8) but function is not usable. Camera will crash if Canon's GetBatteryTemperature is called by CHDK.
+ * To avoid a crash Canon's GetBatteryTemperature must not called. As long CHDK general code do not distinguish between cameras that support or don't support GetBatteryTemperature,
+ * this function will be implemented as specific CHDK-code. It returns always with -99 (which means -99° Celsius or -146° Fahrenheit).
+ * Original entry point in Canon's firmware is disabled (in stubs_entry.S).
+ * */
+int _GetBatteryTemperature()     { return -99;}
+
+/*Auto generated for a810*/
+void *vid_get_bitmap_fb()        { return (void*)0x40411000; }             // Found @0xff8521ac
+void *vid_get_viewport_fb()      { return (void*)0x40566b80; }             // Found @0xffade038
+void *vid_get_viewport_fb_d()    { return (void*)(*(int*)(0x2950+0x54)); } // Found @0xff867ad4 & 0xff867b0c
+char *camera_jpeg_count_str()    { return (char*)0x000b376c; }             // Found @0xff9c9134
+long hook_raw_size()             { return 0x01794300; }                    // Found @0xffae0e6c
+int get_flash_params_count(void) { return 0xa6; }                          // Found @0xff985564

trunk/platform/a810/sub/100d/lib.c
@@ -1,0 +1,47 @@
+#include "platform.h"
+
+char *hook_raw_image_addr()     {return (char*) 0x420B7FC0;}   //Found @0xffae0ef4 a810
+char *hook_alt_raw_image_addr() {return (char*) 0x420B7FC0;}   //Found @0xffae0ef4 a810
+
+#if 0
+//With this implementation, live view works only in some capture modes.
+//Some overlay objects have wrong color
+void *vid_get_viewport_live_fb()
+{
+    return (void*)(*(int*)(0x2094+0x138));		//0xff848398 [0xff8488d8] + 0xff8483f0 a810
+}
+#else
+//With this implementation, live view works in all capture modes
+void *vid_get_viewport_live_fb()
+{
+    return 0x0;
+/*    void **fb=(void **)0x2094;
+    unsigned char buff = *((unsigned char*)0);
+    if (buff == 0) {
+        buff = 2;
+    }
+    else {
+        buff--;
+    }
+    return fb[buff];
+*/
+}
+#endif
+
+/*
+ * Note copied from SX110 IS
+ * GetBatteryTemperature usually will get back temperature of battery compartment/batteries. GetBatteryTemperature is implemented in Canon's firmware for SX120IS.
+ * Firmware entry point is identified (0xFFC394D8) but function is not usable. Camera will crash if Canon's GetBatteryTemperature is called by CHDK.
+ * To avoid a crash Canon's GetBatteryTemperature must not called. As long CHDK general code do not distinguish between cameras that support or don't support GetBatteryTemperature,
+ * this function will be implemented as specific CHDK-code. It returns always with -99 (which means -99° Celsius or -146° Fahrenheit).
+ * Original entry point in Canon's firmware is disabled (in stubs_entry.S).
+ * */
+int _GetBatteryTemperature()     { return -99;}
+
+/*Auto generated for a810*/
+void *vid_get_bitmap_fb()        { return (void*)0x40411000; }             // Found @0xff8521ac
+void *vid_get_viewport_fb()      { return (void*)0x40566b80; }             // Found @0xffade168
+void *vid_get_viewport_fb_d()    { return (void*)(*(int*)(0x2950+0x54)); } // Found @0xff867ad4 & 0xff867b0c
+char *camera_jpeg_count_str()    { return (char*)0x000b3b6c; }             // Found @0xff9c9264
+long hook_raw_size()             { return 0x01794300; }                    // Found @0xffae0f9c
+int get_flash_params_count(void) { return 0xa6; }                          // Found @0xff985670

trunk/core/gui.c
@@ -1882,6 +1882,9 @@ static const char* gui_alt_mode_button_enum(int change, int arg)
 #elif defined(CAMERA_a650)
     static const char* names[]={ "Print", "ISO"};
     static const int keys[]={ KEY_PRINT, KEY_ISO };
+#elif defined(CAMERA_a810)
+    static const char* names[]={ "Playback", "Help"};
+    static const int keys[]={ KEY_PRINT, KEY_HELP };
 #elif defined(CAMERA_a3300) || defined(CAMERA_a3200)
     static const char* names[]={ "Print", "Face", "Disp"};
     static const int keys[]={ KEY_PRINT, KEY_FACE, KEY_DISPLAY };

trunk/include/keyboard.h
@@ -49,6 +49,9 @@
 
 #define KEY_POWER           39
 
+//A810/A1300/A2300/A2400/A4000
+#define KEY_HELP            40
+
 #define JOGDIAL_LEFT        100
 #define JOGDIAL_RIGHT       101
 #define FRONTDIAL_LEFT      102

trunk/include/modelist.h
@@ -129,6 +129,7 @@ enum {
     MODE_BLUR_REDUCTION     , // a800
     MODE_SCN_SOFTFOCUS      , // sx260 asm1989
     MODE_SCN_SMOOTH_SKIN    , // sx260
+    MODE_DIGITAL_IS         , // a810/a2300
 };
 
 #endif

trunk/loader/a810/Makefile
@@ -1,0 +1,42 @@
+curdir=./
+topdir=../../
+
+include $(topdir)makefile.inc
+SUBDIRS=resetcode
+
+LDLIBS=
+LDOPTS=-nostdlib -Wl,--allow-shlib-undefined -Wl,--no-define-common,-EL,-T,$(topdir)tools/link-boot.ld
+LDOPTS+=-Wl,-N,-Ttext,$(MEMBASEADDR)
+
+CPPFLAGS+=-I.
+
+CORE_FILE=$(topdir)core/main.bin
+RESET_FILE=$(curdir)resetcode/main.bin
+
+CPPFLAGS+=-DCORE_FILE=\"$(CORE_FILE)\"
+CPPFLAGS+=-DRESET_FILE=\"$(RESET_FILE)\"
+
+all: all-recursive main.bin
+
+OBJS=entry.o main.o blobs.o
+
+main.bin: main.elf
+	@echo $< \-\> $@
+	$(OBJDUMP) -z -d main.elf > main.dump
+	$(OBJCOPY) -O binary main.elf main.bin
+
+main.elf: $(OBJS)
+	@echo \-\> $@
+	$(CC) $(CFLAGS) -o $@ $^   $(LDLIBS) $(LDFLAGS) $(LDOPTS)
+	( $(NM) $@ | grep ' U ' > $@.syms ) && exit 1 || exit 0
+
+clean: clean-recursive
+	rm -f $(OBJS) main.bin main.elf main.dump main.elf.syms
+
+distclean: distclean-recursive
+	rm -f $(OBJS) main.bin main.elf main.dump main.elf.syms
+	rm -f reboot_core.h copy_and_reset.h
+
+blobs.o: $(RESET_FILE) $(CORE_FILE)
+
+include $(topdir)bottom.inc

trunk/loader/a810/blobs.S
@@ -1,0 +1,28 @@
+    .globl blob_copy_and_reset, blob_copy_and_reset_size
+    .globl blob_chdk_core, blob_chdk_core_size
+
+
+    .section .blob_copy_and_reset
+blob_copy_and_reset_start:
+    .incbin RESET_FILE
+blob_copy_and_reset_end:
+
+
+
+    .section .blob_chdk_core
+blob_chdk_core_start:
+    .incbin CORE_FILE
+blob_chdk_core_end:
+
+
+    .text
+blob_chdk_core_size:
+    .long blob_chdk_core_end - blob_chdk_core_start
+blob_chdk_core:
+    .long blob_chdk_core_start
+
+
+blob_copy_and_reset_size:
+    .long blob_copy_and_reset_end - blob_copy_and_reset_start
+blob_copy_and_reset:
+    .long blob_copy_and_reset_start

commit 19be6e05ccb53639c9b9d2eee8398dd23453917c

fix build error on older GCC version introduced in r2168
git-svn-id: https://subversion.assembla.com/svn/chdk@2171 6794e30b-3f2a-0410-a806-a2bbca1c07ff

trunk/platform/a810/lib.c
@@ -158,7 +158,7 @@ int _rand(void) {
       "AND     %[result], R1, R0, LSR #0x10 \n"
       : [result]"=r"   (value)
       : [input]"m"  (ptr)
-      : /*No clobbers*/
+      : "r0","r1","r2"
 	);
     return value;
 };